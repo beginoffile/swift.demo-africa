9b4ccedc65b5bd153899a3a9dc9c83baee18c751
cesar <cursos.mapatupisaro@gamail.com>
2024-03-19 18:27:58 -0600

    Initial Commit

---
+++ DemoAfrica/DemoAfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAfricaApp.swift
+//  DemoAfrica
+//
+//  Created by Aguid Ramirez Sanchez on 19/03/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoAfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
